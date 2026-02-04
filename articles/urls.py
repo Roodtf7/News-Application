ac03b96d8a2e90f24fec254617c0d2ad2ec20154
Fayyaadh <fayyaadhroodt77@gmail.com>
2026-02-04 02:53:19 +0200

    Add docstrings to articles urls

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Editorial workflows
     path("editor/articles/", views.pending_articles, name="pending-articles"),
     path(
         "editor/articles/<int:article_id>/approve/",
@@ -16,13 +17,16 @@ urlpatterns = [
         views.approve_newsletter,
         name="approve-newsletter"),
 
+    # Feed and details
     path("articles/", views.article_list, name="article-list"),
     path("articles/create/", views.create_article, name="create-article"),
     path("articles/<int:article_id>/", views.article_detail, name="article-detail"),
     path("articles/<int:article_id>/update/", views.update_article, name="update-article"),
     path("articles/<int:article_id>/delete/", views.delete_article, name="delete-article"),
+    
+    # Newsletter management
     path("newsletters/create/", views.create_newsletter, name="create-newsletter"),
     path("newsletters/<int:newsletter_id>/", views.newsletter_detail, name="newsletter-detail"),
     path("newsletters/<int:newsletter_id>/update/", views.update_newsletter, name="update-newsletter"),
     path("newsletters/<int:newsletter_id>/delete/", views.delete_newsletter, name="delete-newsletter"),
-    ]
+]
